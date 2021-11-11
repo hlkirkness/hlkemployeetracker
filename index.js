62d65ad7bfe5215bd997150db10413fab7042b3a
hlkirkness <hlkirkness@gmail.com>
2021-11-10 22:34:34 -0500

    prompts finished

--- index.js
+++ index.js
@@ -48,6 +48,61 @@ const start = () => {
           break;
       }
     });
-  };
 
-  start();
+    const viewAllEmployees = (req, res) => {
+        const sql = `SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "Department Name"
+        FROM employee
+        INNER JOIN role ON employee.role_id = role.id
+        INNER JOIN department ON role.department_id = department.id`;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          start();
+        });
+    };
+      
+    const viewAllDepartments = (req, res) => {
+        const sql = `SELECT* FROM department`;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          start();
+        });
+    };
+      
+    const viewAllRoles = (req, res) => {
+        const sql = `SELECT role.id, role.title, role.salary, department.name as "Department"
+          FROM role
+          INNER JOIN department ON role.department_id = department.id
+          `;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+      
+          console.table(res);
+          mainPrompt();
+        });
+    };
+      
+    const addDepartment = () => {
+        inquirer
+          .prompt([
+            {
+              name: "Name",
+              type: "Input",
+              message: "What is the name of the new Department?",
+            },
+          ])
+          .then((answer) => {
+            const sql = `INSERT INTO department SET ?`;
+            db.query(sql, { name: answer.name }, (err, res) => {
+              if (err) throw err;
+              console.log(answer);
+              start();
+            });
+          });
+    };
+    const addRole = ()=>{
+    }
+};
+
+start();
