1b9c365fdb2e36ec40ad892e34014af7eae41714
hlkirkness <hlkirkness@gmail.com>
2021-11-10 22:42:45 -0500

    view all prompts added

--- app.js
+++ app.js
@@ -52,3 +52,38 @@ const mainPrompt = () => {
       }
     });
 };
+
+const viewAllDepartments = (req, res) => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, res) => {
+      if (err) throw err;
+  
+      console.table(res);
+      mainPrompt();
+    });
+};
+
+const viewAllRoles = (req, res) => {
+    const sql = `SELECT role.id, role.title, role.salary, department.name as "Department"
+    FROM role
+    INNER JOIN department ON role.department_id = department.id
+    `;
+    db.query(sql, (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      mainPrompt();
+    });
+};
+  
+const viewAllEmployees = (req, res) => {
+    const sql = `SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "Department Name", Manager.first_name AS "Manager"
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee Manager ON employee.manager_id = Manager.id`;
+    db.query(sql, (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      mainPrompt();
+    });
+};
